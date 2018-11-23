f1ef29810c5f0d8c7d6d8a5306757e1b67ea1dd6
Oliver Rausch <oliverrausch99@gmail.com>
2018-11-24 00:57:05 +0100

    Update push details

--- dataset/start_docker.sh
+++ dataset/start_docker.sh
@@ -1,1 +1,1 @@
-docker run -v $HOME/docker_data:/var/lib/postgresql -p 25432:5432 --name "postgis" docker-road-accident-prediction:0.1
+docker run -v $HOME/docker_data:/var/lib/postgresql -p 25432:5432 --name "postgis" orausch/docker-road-accident-prediction:0.1
